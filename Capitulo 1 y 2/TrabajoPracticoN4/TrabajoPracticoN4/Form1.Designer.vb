7253b21f97fbfe020900145dc02dd2e8e6c4fdb9
Bwsoat <sosa.ariel.arcenio@gmail.com>
2021-12-03 11:03:35 -0300

    Capitulo 1 y 2 finalizados

---
+++ Capitulo 1 y 2/TrabajoPracticoN4/TrabajoPracticoN4/Form1.Designer.vb
@@ -0,0 +1,196 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TrabajoPracticoN4
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.inputNum1 = New System.Windows.Forms.TextBox()
+        Me.inputNum2 = New System.Windows.Forms.TextBox()
+        Me.btnMultiplicar = New System.Windows.Forms.Button()
+        Me.btnRestar = New System.Windows.Forms.Button()
+        Me.btnDividir = New System.Windows.Forms.Button()
+        Me.btnCerrar = New System.Windows.Forms.Button()
+        Me.btnTotal = New System.Windows.Forms.Button()
+        Me.btnPotencia = New System.Windows.Forms.Button()
+        Me.btnSumar = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 14)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(238, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Ingrese un Numero:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 59)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(257, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Ingrese otro Numero:"
+        '
+        'inputNum1
+        '
+        Me.inputNum1.Location = New System.Drawing.Point(278, 14)
+        Me.inputNum1.Margin = New System.Windows.Forms.Padding(5)
+        Me.inputNum1.Name = "inputNum1"
+        Me.inputNum1.Size = New System.Drawing.Size(172, 32)
+        Me.inputNum1.TabIndex = 2
+        '
+        'inputNum2
+        '
+        Me.inputNum2.Location = New System.Drawing.Point(278, 59)
+        Me.inputNum2.Margin = New System.Windows.Forms.Padding(5)
+        Me.inputNum2.Name = "inputNum2"
+        Me.inputNum2.Size = New System.Drawing.Size(172, 32)
+        Me.inputNum2.TabIndex = 3
+        '
+        'btnMultiplicar
+        '
+        Me.btnMultiplicar.Location = New System.Drawing.Point(538, 98)
+        Me.btnMultiplicar.Margin = New System.Windows.Forms.Padding(5)
+        Me.btnMultiplicar.Name = "btnMultiplicar"
+        Me.btnMultiplicar.Size = New System.Drawing.Size(150, 52)
+        Me.btnMultiplicar.TabIndex = 4
+        Me.btnMultiplicar.Text = "&Multiplicar"
+        Me.btnMultiplicar.UseVisualStyleBackColor = True
+        '
+        'btnRestar
+        '
+        Me.btnRestar.Location = New System.Drawing.Point(794, 19)
+        Me.btnRestar.Margin = New System.Windows.Forms.Padding(5)
+        Me.btnRestar.Name = "btnRestar"
+        Me.btnRestar.Size = New System.Drawing.Size(150, 52)
+        Me.btnRestar.TabIndex = 5
+        Me.btnRestar.Text = "&Restar"
+        Me.btnRestar.UseVisualStyleBackColor = True
+        '
+        'btnDividir
+        '
+        Me.btnDividir.Location = New System.Drawing.Point(794, 98)
+        Me.btnDividir.Margin = New System.Windows.Forms.Padding(5)
+        Me.btnDividir.Name = "btnDividir"
+        Me.btnDividir.Size = New System.Drawing.Size(150, 52)
+        Me.btnDividir.TabIndex = 6
+        Me.btnDividir.Text = "&Dividir"
+        Me.btnDividir.UseVisualStyleBackColor = True
+        '
+        'btnCerrar
+        '
+        Me.btnCerrar.Location = New System.Drawing.Point(827, 309)
+        Me.btnCerrar.Margin = New System.Windows.Forms.Padding(5)
+        Me.btnCerrar.Name = "btnCerrar"
+        Me.btnCerrar.Size = New System.Drawing.Size(150, 52)
+        Me.btnCerrar.TabIndex = 7
+        Me.btnCerrar.Text = "&Cerrar"
+        Me.btnCerrar.UseVisualStyleBackColor = True
+        '
+        'btnTotal
+        '
+        Me.btnTotal.BackColor = System.Drawing.Color.Red
+        Me.btnTotal.ForeColor = System.Drawing.Color.White
+        Me.btnTotal.Location = New System.Drawing.Point(264, 139)
+        Me.btnTotal.Margin = New System.Windows.Forms.Padding(5)
+        Me.btnTotal.Name = "btnTotal"
+        Me.btnTotal.Size = New System.Drawing.Size(150, 52)
+        Me.btnTotal.TabIndex = 8
+        Me.btnTotal.Text = "Total"
+        Me.btnTotal.UseVisualStyleBackColor = False
+        '
+        'btnPotencia
+        '
+        Me.btnPotencia.Location = New System.Drawing.Point(667, 188)
+        Me.btnPotencia.Margin = New System.Windows.Forms.Padding(5)
+        Me.btnPotencia.Name = "btnPotencia"
+        Me.btnPotencia.Size = New System.Drawing.Size(150, 52)
+        Me.btnPotencia.TabIndex = 9
+        Me.btnPotencia.Text = "&Potencia"
+        Me.btnPotencia.UseVisualStyleBackColor = True
+        '
+        'btnSumar
+        '
+        Me.btnSumar.Location = New System.Drawing.Point(538, 19)
+        Me.btnSumar.Margin = New System.Windows.Forms.Padding(5)
+        Me.btnSumar.Name = "btnSumar"
+        Me.btnSumar.Size = New System.Drawing.Size(150, 52)
+        Me.btnSumar.TabIndex = 10
+        Me.btnSumar.Text = "&Sumar"
+        Me.btnSumar.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(21, 156)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(124, 25)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Resultado"
+        '
+        'TrabajoPracticoN4
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(14.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(1005, 395)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnSumar)
+        Me.Controls.Add(Me.btnPotencia)
+        Me.Controls.Add(Me.btnTotal)
+        Me.Controls.Add(Me.btnCerrar)
+        Me.Controls.Add(Me.btnDividir)
+        Me.Controls.Add(Me.btnRestar)
+        Me.Controls.Add(Me.btnMultiplicar)
+        Me.Controls.Add(Me.inputNum2)
+        Me.Controls.Add(Me.inputNum1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(5)
+        Me.Name = "TrabajoPracticoN4"
+        Me.Text = "TrabajoPracticoN4"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents inputNum1 As System.Windows.Forms.TextBox
+    Friend WithEvents inputNum2 As System.Windows.Forms.TextBox
+    Friend WithEvents btnMultiplicar As System.Windows.Forms.Button
+    Friend WithEvents btnRestar As System.Windows.Forms.Button
+    Friend WithEvents btnDividir As System.Windows.Forms.Button
+    Friend WithEvents btnCerrar As System.Windows.Forms.Button
+    Friend WithEvents btnTotal As System.Windows.Forms.Button
+    Friend WithEvents btnPotencia As System.Windows.Forms.Button
+    Friend WithEvents btnSumar As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ Capitulo 1 y 2/TrabajoPracticoN4/TrabajoPracticoN4/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class TrabajoPracticoN4
+
+    Private Sub btnCerrar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCerrar.Click
+        'Cerramos la app
+        End
+    End Sub
+
+    Private Sub btnSumar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSumar.Click
+        'Sumas los dos valores
+        btnTotal.Text = Val(inputNum1.Text) + Val(inputNum2.Text)
+
+    End Sub
+
+    Private Sub btnRestar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnRestar.Click
+        'Restamos los dos valores
+        btnTotal.Text = Val(inputNum1.Text) - Val(inputNum2.Text)
+    End Sub
+
+    Private Sub btnMultiplicar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnMultiplicar.Click
+        'Multiplicamos los dos valores
+        btnTotal.Text = Val(inputNum1.Text) * Val(inputNum2.Text)
+    End Sub
+
+    Private Sub btnDividir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDividir.Click
+        'Dividimos los dos valores
+        btnTotal.Text = Val(inputNum1.Text) / Val(inputNum2.Text)
+    End Sub
+
+    Private Sub btnPotencia_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnPotencia.Click
+        'Potencia de los dos valores
+        btnTotal.Text = Val(inputNum1.Text) ^ Val(inputNum2.Text)
+    End Sub
+End Class
